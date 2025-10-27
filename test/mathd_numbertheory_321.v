8b261445f91ce9bd52f3ce602001218e18492754
Jules Viennot Franca <jules.viennot@gmail.com>
2025-10-27 15:11:46 +0100

    mathd_numbertheory_321 fixed

--- test/mathd_numbertheory_321.v
+++ test/mathd_numbertheory_321.v
@@ -3,7 +3,7 @@ Open Scope Z_scope.
 
 Theorem mathd_numbertheory_321:
   forall n : Z,
-  (1 <= n <= 1399) /\ (n * 160 mod 1399 = 1) ->
+  (0 <= n < 1399) /\ (n * 160 mod 1399 = 1) ->
   n = 1058.
 Proof.
 Admitted.
